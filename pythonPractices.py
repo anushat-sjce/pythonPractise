094e861728a2672990fa55ec62a3e2f5477e29ed
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-05-30 17:46:03 +0530

    Create pythonPractices.py

---
+++ pythonPractices.py
@@ -0,0 +1,37 @@
+def hello():
+  print("Hello world")
+
+print(2 * 3 ** 2)
+
+num = ["bulb", "light", "switch", "wires"]
+num
+num.pop()
+num
+
+values.insert(1, 0)
+values
+values.clear()
+values
+
+values = [ 10, 20, 30, 40]
+for i in values:
+  print(i/2)
+print("Another method")
+[i/8 for i in values]
+
+values[0:5]
+
+for i in basket:
+  print(i)
+
+items = ['Onion', 'Carrot', 'Beans', 'Garlic', 'Brinjal']
+len(items)
+
+items.clear()
+
+del items[0]
+
+#items.remove("Brinjal")
+#items.append("Brinjal")
+x = items.pop
+print(x)
