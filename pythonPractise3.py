07e54096f8ab3d36c606e9da5855c0b13f3c6da5
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-01 12:52:47 +0530

    Create pythonPractise3.py

---
+++ pythonPractise3.py
@@ -0,0 +1,8 @@
+def hello(name):
+  print(f"Hello {name}")
+name = "Ishaan and Anusha and Jay"
+hello(name)
+
+Country = "Australia"
+Capital = "Canberra"
+print(f"{Capital} is the capital of {Country}")
