8504bca81e0f19e76a1f648aa1fffac03a5a5bc8
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-01 12:57:15 +0530

    Update pythonPractise3.py

--- pythonPractise3.py
+++ pythonPractise3.py
@@ -6,3 +6,14 @@ hello(name)
 Country = "Australia"
 Capital = "Canberra"
 print(f"{Capital} is the capital of {Country}")
+
+
+
+### Adding number and returning the value
+def add(num1, num2):
+ # print(num1 + num2)
+  return num1+ num2
+num1 = 64
+num2 = 46
+v = add(num1, num2)
+print(v)
