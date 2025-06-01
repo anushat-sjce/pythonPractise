a88f08ca58e995231653f21be8cd63664bb5939f
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-06-01 14:05:37 +0530

    Update pythonPractise3.py

--- pythonPractise3.py
+++ pythonPractise3.py
@@ -9,7 +9,7 @@ print(f"{Capital} is the capital of {Country}")
 
 
 
-### Adding number and returning the value
+*************************************### Adding number and returning the value
 def add(num1, num2):
  # print(num1 + num2)
   return num1+ num2
@@ -17,3 +17,20 @@ num1 = 64
 num2 = 46
 v = add(num1, num2)
 print(v)
+
+n = 23
+********************************nested functions***************************
+def test(n):
+  n = 20
+  print(n)
+
+test(19)
+
+def hello(name):
+  print(f"Hello {name}")
+
+def fav_city(city_name,name):
+  hello(name)
+  print(f"Welcome to {city_name}!!")
+
+fav_city("Australia", "Arthi")
