30925612ae8e1d3cb867f89a3a81db168d4dde4b
Anusha T <80740616+anushat-sjce@users.noreply.github.com>
2025-07-18 16:42:22 +0530

    Update python_practise7.py

--- python_practise7.py
+++ python_practise7.py
@@ -29,3 +29,27 @@ for keys in Inventory_store.keys():
         del Inventory_store[keys]
         break
 print(Inventory_store)
+
+
+A = [1,2,2,1]
+B = [2,2,1,1,2]
+if (sum(A) == sum(B)):
+    print("True")
+else:
+    print("False")
+    
+    
+
+album_set1 = set(["Thriller", 'AC/DC', 'Back in Black'])
+album_set2 = set([ "AC/DC", "Back in Black", "The Dark Side of the Moon"])
+album_set3 = album_set1.union(album_set2)
+print(album_set3)
+
+x = album_set1.issubset(album_set3)
+print(x)
+
+y = album_set1.issuperset({"Back in Black", "AC/DC"})
+print(y)
+
+A = set(['rap','house','electronic music', 'rap'])
+print(A)
